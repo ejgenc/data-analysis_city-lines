0db740cf98039c8958a5467c62ddc317d08e78e6
ejgenc@gmail.com <ejgenc@gmail.com>
2021-12-04 11:54:42 +0100

    Fleshed out 'environment.yml' and 'setup.py'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='1.0',
+    description=('Code for the "Information Structures and Implications" class'
+                 'final assignment'),
+    author='Nisa Irem Kırbaç, Eren Janberk Genç',
+    license='MIT',
+)
